4569972ef59a43393f120f6c16a6f7d68d88d9c0
Vladimir <vovach777@yandex.ru>
2022-11-06 20:18:51 +0300

    Update my.c

--- my.c
+++ my.c
@@ -31,14 +31,14 @@ typedef struct TagDHTree {
 
 static void TruncatedBinary(unsigned size, unsigned *k, unsigned *u) {
 /* If n is a power of two, then the coded value for 0 ≤ x < n is the simple binary code for x of length log2(n).
-    Otherwise let k = floor(log2(n)), such that 2k < n < 2k+1 and let u = 2k+1 − n.  */
+	Otherwise let k = floor(log2(n)), such that 2k < n < 2k+1 and let u = 2k+1 − n.  */
 	// Set k = floor(log2(n)), i.e., k such that 2^k <= n < 2^(k+1).
-	int kl = 0, t = size;
-	while (t > 1) { kl++;  t >>= 1; }
-   *k=kl;
+	unsigned kk = 0, t = size;
+	while (t > 1) { kk++;  t >>= 1; }
+	*k=kk;
 
 	// Set u to the number of unused codewords = 2^(k+1) - n.
-	*u = (1 << kl + 1) - size;
+	*u = (1 << kk + 1) - size;
 }
 
 void init_dh(DHTree ctx, unsigned size) {
